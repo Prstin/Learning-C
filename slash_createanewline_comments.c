42ef68b89d71fddb80a33cb9f1f4c254f4c6b6e6
prstin <prstiv@proton.me>
2024-01-17 20:39:28 +0530

    adding all the files

---
+++ slash_createanewline_comments.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+printf("Hey who are you?\n");
+printf("I am ryu\n");
+// \n is used to make new line.
+printf("Cool but isnt it too hot\n is what i\n am thinking");
+
+printf("\n\n");
+
+// if \n command used twice it creates a blank space between two lines. (leave a line)
+printf("haha help me.\n \nNo I wont.");
+
+printf("\n \n");
+
+// \t is used to leave a space between two lines 
+printf("why not.\t why would I?");
+
+printf("\n\n");
+
+// command \\ is used as "or".It creates a slash. (inserts a blackslash. src=w3)
+printf("cuz i dont like\\hate you\n");
+printf("cringe mf");
+
+printf("\n\n");
+
+//you cant    printf("yo imma name John spelled as "Jean" understood?") it will give an error. So you write
+printf("yo imma name John spelled as \"Jean\"you understood?");
+
+/* write everything between these
+ and  it will be ignored */
+
+return 0;
+}
+
---
+++ variable.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* int stores integer numbers (not decimals) 167
+   float stores decimal numberes             167.02
+   char stores characters such as 'help' 'dont' 
+        char are surrounded by single quotes = '
+*/
+
+
+/* syntax = 
+type variableName = value;  
+
+if
+int main() {
+  int myNum = 78;
+  printf(myNum);
+  return 0;
+} 
+
+this thing wont work */
+
+int main () {
+   int myNum = 89;
+   printf("%d", myNum);
+   return 0; }
+
+
